c70e92a545a268f2538f47dba60759196370e34a
Elle FL <sage.fio.lange@gmail.com>
2025-01-24 19:59:34 -0500

    about

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <main className="sm:flex sm:max-w-md sm:h-full sm:items-center max-sm:px-4 max-sm:pt-12">
+      <h1 className="max-sm:text-4xl">Elle Fiorentino-Lange</h1>
+      <p className="font-cormorant max-w-md">
+        is an artist based in Boston, Massachusetts. Her work is centered on
+        data, transformation, and metaphysics.
+      </p>
+    </main>
+  );
+}
