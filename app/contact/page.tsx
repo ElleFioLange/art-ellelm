6fbb66923d58dbe7a9fb3154bd4548bb69c141a2
Elle FL <sage.fio.lange@gmail.com>
2025-02-01 11:02:02 -0500

    improve contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,9 +5,6 @@ import {
   ReactSketchCanvas,
   ReactSketchCanvasRef,
 } from "react-sketch-canvas";
-
-import useViewport from "@/utils/useViewport";
-import useTheme from "@/utils/useTheme";
 import { useRef, useState } from "react";
 
 export default function Contact() {
@@ -17,12 +14,6 @@ export default function Contact() {
 
   const canvasRef = useRef<ReactSketchCanvasRef>(null);
 
-  const { w: _w, h: _h } = useViewport();
-  // This gives w and h as 0 if they are undefined
-  const { w, h } = { w: _w || 0, h: _h || 0 };
-
-  const theme = useTheme();
-
   const handleSend = () => {
     open[1](false);
     setTimeout(() => sent[1](true), 1000);
@@ -30,16 +21,18 @@ export default function Contact() {
 
   return (
     <main className="font-cormorant w-full h-full flex justify-center gap-2 items-center flex-col p-4">
-      To get in contact, please email me at{" "}
-      <a href="mailto:elle.fio.lange@gmail.com">elle.fio.lange@gmail.com</a> or
-      you can send me a message below:
+      To get in contact, please email me at
+      <a className="underline" href="mailto:elle.fio.lange@gmail.com">
+        elle.fio.lange@gmail.com
+      </a>{" "}
+      or you can send me a message below:
       <div
         className={`w-full h-px bg-fg max-w-xl ease-in-out transition-all duration-1000 ${
           open[0] ? "opacity-100" : "opacity-0"
         }`}
       />
       <div
-        className={`w-full max-w-xl h-full ${
+        className={`w-full select-none max-w-xl h-full ${
           open[0] ? "max-h-96 opacity-100 mb-0" : "max-h-0 opacity-0 -mt-4"
         } transition-all duration-1000 ease-in-out relative`}
       >
