83f5fd9546ec432f7e26d38126d2976998fbdddc
Elle FL <sage.fio.lange@gmail.com>
2025-02-06 23:37:01 -0500

    sculpture content minus text

---
+++ app/(galleries)/sculpture/page.tsx
@@ -0,0 +1,68 @@
+import Gallery from "../gallery";
+import Image from "next/image";
+
+export default function Sculptures() {
+  return (
+    <Gallery>
+      <div className="sm:w-[40vw] sm:h-[40vw]">
+        <Image
+          src="/sculpture/point-cloud/point-cloud.png"
+          width={1080}
+          height={1051}
+          alt="Human Activities Along the Planar Continuum of Man, Woman, and Human From the Perspective of Artificial Intelligence"
+        />
+        <div>
+          <h1>
+            Human Activities Along the Planar Continuum of Man, Woman, and Human
+            From the Perspective of Artificial Intelligence
+          </h1>
+          <h2>In Progress</h2>
+          <h3>3D Printed Resin</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[9vw]">
+        <Image
+          src="/sculpture/image/2.png"
+          width={1080}
+          height={229}
+          alt="Image II"
+        />
+        <div>
+          <h1>Image II</h1>
+          <h2>2024</h2>
+          <h3>Hard Drive Platters & Mirror</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[32vw]">
+        <Image
+          src="/sculpture/image/1.jpg"
+          width={1080}
+          height={884}
+          alt="Image I"
+        />
+        <div>
+          <h1>Image I</h1>
+          <h2>2024</h2>
+          <h3>Hard Drive Platters & Mirror</h3>
+        </div>
+      </div>
+
+      {/* Putting this in the sculpture category for now, but planning to move it later */}
+      <div className="sm:w-[32vw] sm:h-[32vw]">
+        <Image
+          src="/sculpture/grow-n.png"
+          width={927}
+          height={917}
+          alt="Grow of N"
+        />
+        <div>
+          <h1>Grow(N)</h1>
+          <h2>2024</h2>
+          <h3>Printed Book</h3>
+        </div>
+      </div>
+    </Gallery>
+  );
+}
