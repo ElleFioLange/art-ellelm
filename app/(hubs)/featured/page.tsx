14b08117859e35d5433ceb93b3b31e0bcb0323b5
Elle FL <sage.fio.lange@gmail.com>
2025-02-07 14:55:25 -0500

    video for featured hub

--- app/(hubs)/featured/page.tsx
+++ app/(hubs)/featured/page.tsx
@@ -5,8 +5,24 @@ import Image from "next/image";
 export default function Featured() {
   return (
     <>
+      <a href="/time-space">
+        <video
+          src="/computational/time-space/globe-light-glass.mp4"
+          width={1280}
+          height={720}
+          autoPlay
+          muted
+          loop
+        />
+        <h2>Time-Space</h2>
+      </a>
       <a href="/image">
-        <Image src="/sculpture/image/1.jpg" alt="" width={1080} height={884} />
+        <Image
+          src="/sculpture/image/1.jpg"
+          alt="Image I"
+          width={1080}
+          height={884}
+        />
         <h2>Image</h2>
       </a>
     </>
