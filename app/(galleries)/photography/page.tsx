4d23dea77f4a97e0d17c962478194029a467ac06
Elle FL <sage.fio.lange@gmail.com>
2025-02-06 23:06:23 -0500

    photography content minus text

--- app/(galleries)/photography/page.tsx
+++ app/(galleries)/photography/page.tsx
@@ -1,3 +1,451 @@
+import Link from "next/link";
+import Gallery from "../gallery";
+import Image from "next/image";
+
 export default function Photography() {
-  return null;
+  return (
+    <Gallery>
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/light-painting/1.jpg"
+          width={1080}
+          height={810}
+          alt="Untitled"
+        />
+        <div>
+          <h1>Untitled</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/light-painting/2.jpg"
+          width={1080}
+          height={810}
+          alt="Untitled"
+        />
+        <div>
+          <h1>Untitled</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/piano-summer.jpg"
+          width={1080}
+          height={810}
+          alt="Piano's Spirit (Summer)"
+        />
+        <div>
+          <h1>Piano's Spirit (Summer)</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/piano-winter.jpg"
+          width={1080}
+          height={810}
+          alt="Piano's Spirit (Winter)"
+        />
+        <div>
+          <h1>Piano's Spirit (Winter)</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/piano-nuclear.jpg"
+          width={1080}
+          height={810}
+          alt="Piano's Spirit (Nuclear Explosion)"
+        />
+        <div>
+          <h1>Piano's Spirit (Nuclear Explosion)</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/angels-glow-clouds.jpg"
+          width={1080}
+          height={810}
+          alt="Angels Glow by Floating Clouds"
+        />
+        <div>
+          <h1>Angels Glow by Floating Clouds</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[32vw] sm:h-[40vw]">
+        <Image
+          src="/photography/assistive-vision/3.jpg"
+          width={862}
+          height={1080}
+          alt="Assistive Vision 3"
+        />
+        <div>
+          <h1>Assistive Vision 3</h1>
+          <h2>2024</h2>
+          <h3>Software Enhanced Photography</h3>
+          <p>
+            This piece is part of a{" "}
+            <Link href="/assistive-vision">series.</Link>
+          </p>
+        </div>
+      </div>
+
+      <div className="sm:w-[32vw] sm:h-[40vw]">
+        <Image
+          src="/photography/assistive-vision/4.jpg"
+          width={862}
+          height={1080}
+          alt="Assistive Vision 4"
+        />
+        <div>
+          <h1>Assistive Vision 4</h1>
+          <h2>2024</h2>
+          <h3>Software Enhanced Photography</h3>
+          <p>
+            This piece is part of a{" "}
+            <Link href="/assistive-vision">series.</Link>
+          </p>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/instant-60mph.jpg"
+          width={1080}
+          height={810}
+          alt="The Instant of 60 Miles Per Hour"
+        />
+        <div>
+          <h1>The Instant of 60 Miles Per Hour</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/palette-wind-leaves.jpg"
+          width={1080}
+          height={810}
+          alt="Palette Composition (Wind & Leaves)"
+        />
+        <div>
+          <h1>Palette Composition (Wind & Leaves)</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/horror-brick.png"
+          width={1080}
+          height={810}
+          alt="Horror Ripped From Underneath Brick"
+        />
+        <div>
+          <h1>Horror Ripped From Underneath Brick</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[20vw] sm:h-[40vw]">
+        <Image
+          src="/photography/hancock-tower.jpg"
+          width={555}
+          height={1080}
+          alt="Hancock Tower, Courtesy Apple Inc."
+        />
+        <div>
+          <h1>Hancock Tower, Courtesy Apple Inc.</h1>
+          <h2>2024</h2>
+          <h3>Screenshot</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/tree-canopy.jpg"
+          width={1080}
+          height={810}
+          alt="Tree (Roots Resourced From Canopy)"
+        />
+        <div>
+          <h1>Tree (Roots Resourced From Canopy)</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[16vw] sm:h-[40vw]">
+        <Image
+          src="/photography/lit-tower.jpg"
+          width={440}
+          height={1080}
+          alt="Lit Tower (View From Train)"
+        />
+        <div>
+          <h1>Lit Tower (View From Train)</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[32vw] sm:h-[40vw]">
+        <Image
+          src="/photography/mountain-bedsheet.jpg"
+          width={1080}
+          height={810}
+          alt="Mountain Beneath Bedsheet"
+        />
+        <div>
+          <h1>Mountain Beneath Bedsheet</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[60vw] sm:h-[45vw]">
+        <Image
+          src="/photography/weightless.jpg"
+          width={1080}
+          height={810}
+          alt="The Miniscule Nature of Weightlessness"
+        />
+        <div>
+          <h1>The Miniscule Nature of Weightlessness</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[30vw] sm:h-[40vw]">
+        <Image
+          src="/photography/train-waves.jpg"
+          width={1080}
+          height={810}
+          alt="Train Window as Ocean Waves"
+        />
+        <div>
+          <h1>Train Window as Ocean Waves</h1>
+          <h2>2024</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[40vw]">
+        <Image
+          src="/photography/search-inside-image.jpg"
+          width={1125}
+          height={1143}
+          alt="Search Inside Image"
+        />
+        <div>
+          <h1>Search Inside Image</h1>
+          <h2>2024</h2>
+          <h3>Screenshot</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[31vw]">
+        <Image
+          src="/photography/exposure-chin.jpg"
+          width={1080}
+          height={842}
+          alt="Exposure of the Chin in the Arms of an Angel"
+        />
+        <div>
+          <h1>Exposure of the Chin in the Arms of an Angel</h1>
+          <h2>2023</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[25vw]">
+        <Image
+          src="/photography/castle-occluded.jpg"
+          width={1080}
+          height={685}
+          alt="Castle Occluded by Umbrella and Tempest"
+        />
+        <div>
+          <h1>Castle Occluded by Umbrella and Tempest</h1>
+          <h2>2023</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[48vw] sm:h-[24vw]">
+        <Image
+          src="/photography/seeds-winter.jpg"
+          width={1080}
+          height={534}
+          alt="What Seeds Are in Winter"
+        />
+        <div>
+          <h1>What Seeds Are in Winter</h1>
+          <h2>2023</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[23vw] sm:h-[40vw]">
+        <Image
+          src="/photography/symbols-relativity.jpg"
+          width={625}
+          height={1080}
+          alt="Symbols in Stone are Still Subjects of Relativity"
+        />
+        <div>
+          <h1>Symbols in Stone are Still Subjects of Relativity</h1>
+          <h2>2023</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[32vw] sm:h-[32vw]">
+        <Image
+          src="/photography/movement-color.jpg"
+          width={1080}
+          height={1002}
+          alt="Movement Is a Thing for Capturing Color"
+        />
+        <div>
+          <h1>Movement Is a Thing for Capturing Color</h1>
+          <h2>2023</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[48vw] sm:h-[24vw]">
+        <Image
+          src="/photography/loudest-moments.jpg"
+          width={1080}
+          height={524}
+          alt="The Loudest Moments Move Without Color"
+        />
+        <div>
+          <h1>The Loudest Moments Move Without Color</h1>
+          <h2>2023</h2>
+          <h3>Digital Photography & Manipulation</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/ducks.jpg"
+          width={1080}
+          height={824}
+          alt="Ducks"
+        />
+        <div>
+          <h1>Ducks</h1>
+          <h2>2023</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[32vw] sm:h-[32vw]">
+        <Image
+          src="/photography/self-portraits/1.jpg"
+          width={1080}
+          height={1080}
+          alt="Self-Portrait I"
+        />
+        <div>
+          <h1>Self-Portrait I</h1>
+          <h2>2023</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[32vw] sm:h-[32vw]">
+        <Image
+          src="/photography/self-portraits/2.jpg"
+          width={1080}
+          height={1080}
+          alt="Self-Portrait II"
+        />
+        <div>
+          <h1>Self-Portrait II</h1>
+          <h2>2023</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/mt-rainier.jpg"
+          width={1080}
+          height={810}
+          alt="Mt. Rainier"
+        />
+        <div>
+          <h1>Mt. Rainier</h1>
+          <h2>2020</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/horizon.jpg"
+          width={1080}
+          height={810}
+          alt="Horizon"
+        />
+        <div>
+          <h1>Horizon</h1>
+          <h2>2020</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/four-corners.jpg"
+          width={1080}
+          height={810}
+          alt="Untitled"
+        />
+        <div>
+          <h1>Untitled</h1>
+          <h2>2020</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+
+      <div className="sm:w-[40vw] sm:h-[30vw]">
+        <Image
+          src="/photography/four-corners-2.jpg"
+          width={1080}
+          height={810}
+          alt="Untitled"
+        />
+        <div>
+          <h1>Untitled</h1>
+          <h2>2020</h2>
+          <h3>Digital Photography</h3>
+        </div>
+      </div>
+    </Gallery>
+  );
 }
