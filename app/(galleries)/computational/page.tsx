456b2d63e95160e4c588d2d5f8874c86e689fd41
Elle FL <sage.fio.lange@gmail.com>
2025-02-07 14:15:48 -0500

    computational gallery changes

--- app/(galleries)/computational/page.tsx
+++ app/(galleries)/computational/page.tsx
@@ -4,6 +4,20 @@ import Gallery from "../gallery";
 export default function Computational() {
   return (
     <Gallery>
+      <div className="sm:w-[64vw] sm:h-[36vw]">
+        <Image
+          src="/computational/time-space/globe-light-glass.jpg"
+          width={1280}
+          height={720}
+          alt="Time-Space: Globe, Light, Glass"
+        />
+        <div>
+          <h1>Time-Space: Globe, Light, Glass</h1>
+          <h2>2024</h2>
+          <h3>Computational Video</h3>
+        </div>
+      </div>
+
       <div className="sm:w-[60vw] sm:h-[60vw]">
         <video
           src="/multiple/point-cloud/render.mp4"
@@ -14,7 +28,7 @@ export default function Computational() {
           loop
         />
         <div>
-          <h1>
+          <h1 className="max-sm:text-4xl">
             Human Activities Along the Planar Continuum of Man, Woman, and Human
             From the Perspective of Artificial Intelligence
           </h1>
@@ -53,20 +67,6 @@ export default function Computational() {
           </h1>
           <h2>2021</h2>
           <h3>computational Image</h3>
-        </div>
-      </div>
-
-      <div className="sm:w-[64vw] sm:h-[36vw]">
-        <Image
-          src="/computational/time-space/globe-light-glass.jpg"
-          width={1280}
-          height={720}
-          alt="Time-Space: Globe, Light, Glass"
-        />
-        <div>
-          <h1>Time-Space: Globe, Light, Glass</h1>
-          <h2>2024</h2>
-          <h3>Computational Video</h3>
         </div>
       </div>
     </Gallery>
