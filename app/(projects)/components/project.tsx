675929f4d8fa304be336563f87c233e69dbe4666
Elle FL <sage.fio.lange@gmail.com>
2025-02-08 18:50:10 -0500

    don't need project layout

--- app/(projects)/components/project.tsx
+++ app/(projects)/components/project.tsx
@@ -58,9 +58,9 @@ export default function Project({ children }: { children: ReactNode }) {
   text.pop();
 
   return (
-    <>
+    <main className="min-h-0 min-w-0 w-full h-full max-h-dvh overflow-hidden mx-auto max-w-screen-lg grid gap-4 items-center lg:w-3/4 sm:grid-cols-[minmax(auto,1fr),1px,minmax(auto,1fr)] sm:px-8 max-sm:grid-rows-[minmax(auto,40%),1px,1fr] max-sm:py-4">
       <Text picturesRef={picturesRef} text={text} />
       <Pictures ref={picturesRef} pictures={pictures} />
-    </>
+    </main>
   );
 }
