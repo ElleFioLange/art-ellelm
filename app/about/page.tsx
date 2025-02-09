fecd546f6c4d8a656169623bcc8bf1ea477ba9a3
Elle FL <sage.fio.lange@gmail.com>
2025-02-08 22:10:27 -0500

    add influences to about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,108 @@
 export default function About() {
   return (
-    <main className="h-full w-full flex sm:items-center justify-center p-4">
-      <section className="max-w-xl max-h-min">
+    <main className="h-full w-full overflow-auto flex justify-center p-4">
+      <section className="max-w-xl sm:mt-[32dvh] sm:mb-[16dvh] max-sm:mt-[16dvh] max-sm:mb-[8dvh] h-min [&>p]:mb-1">
         <h1 className="max-sm:text-4xl">Elle Fiorentino-Lange</h1>
-        <h2 className="">is an artist based in Boston, MA.</h2>
+        <h2>is an artist based in Boston, MA.</h2>
         <div className="w-full h-px bg-fg my-2" />
-        <p>Her work is centered on data, transformation, and metaphysics.</p>
+        <p>
+          Her work is centered on data, transformation, metaphor and
+          metaphysics.
+        </p>
+        <p>
+          Here is a list she keeps of art and artists who have impacted or
+          inspired her. This list is in no specific order.
+        </p>
+        <ul className="font-cormorant list-disc ml-4">
+          <li>Johan Ripjma</li>
+          <li>John Cage</li>
+          <li>Cy Twombly </li>
+          <li>Martin Venezky</li>
+          <li>Hilma af Klint </li>
+          <li>Joan of Arc</li>
+          <li>Agnes Martin </li>
+          <li>Egon Schiele</li>
+          <li>Max Cooper</li>
+          <li>SOPHIE</li>
+          <li>A.G. Cook</li>
+          <li>Contrapoints</li>
+          <li>Lu Wilson </li>
+          <li>Joan Miró </li>
+          <li>
+            <i>Orlando</i> by Virginia Wolff
+          </li>
+          <li>
+            <i>House of the Scorpion</i> by Nancy Farmer
+          </li>
+          <li>
+            <i>Tinkers</i> by Paul Harding
+          </li>
+          <li>Trevor Paglen</li>
+          <li>
+            <i>Eager</i> by Helen Fox{" "}
+          </li>
+          <li>
+            <i>Things Fall Apart</i> by Chinua Achebe
+          </li>
+          <li>John Archibald Wheeler - “It from bit”</li>
+          <li>
+            <i>Detransition Baby</i> by Torrey Peters
+          </li>
+          <li>Iris van Herpen</li>
+          <li>Olafur Eliasson</li>
+          <li>James Turrell</li>
+          <li>Reuben Margolin</li>
+          <li>Jamie Nares</li>
+          <li>Rubeen Salem</li>
+          <li>Miranda July</li>
+          <li>Francis Alys</li>
+          <li>Wangechi Mutu</li>
+          <li>Kate Darling</li>
+          <li>Kosei Komatsu</li>
+          <li>William Darrell</li>
+          <li>
+            <i>Conversation</i> by Max Krieger
+          </li>
+          <li>Cannupa Hanska Luger</li>
+          <li>Benjamin Betts</li>
+          <li>
+            <i>Doppelgängers</i> by Naomi Klein
+          </li>
+          <li>Irene Shawinsky</li>
+          <li>Jordan Wolfson</li>
+          <li>
+            <i>Reality Has No Fundamental Level</i> by Ross Cameron
+          </li>
+          <li>Angiola Gatti</li>
+          <li>Joshua Serafin</li>
+          <li>
+            <i>CCRU</i>
+          </li>
+          <li>Felix Gonzalez-Torres</li>
+          <li>Annea Lockwood</li>
+          <li>Jenny Holzer</li>
+          <li>Ivana Bašić</li>
+          <li>Laurie Anderson</li>
+          <li>Joshua Citarella</li>
+          <li>Emily Dickinson</li>
+          <li>Joscha Bach</li>
+          <li>Vera Molnar</li>
+          <li>Dexter Callender III</li>
+          <li>Dave Brubeck</li>
+          <li>Vegyn</li>
+          <li>Yoko Ono</li>
+          <li>Do Not Research</li>
+          <li>Alexey Firstov</li>
+          <li>Rebecca from Soft White Underbelly</li>
+          <li>Frank Lloyd Wright</li>
+          <li>Do Ho Suh</li>
+          <li>Tau Lewis</li>
+          <li>Ethel Cain </li>
+          <li>Pina Bausch</li>
+          <li>Andrea Fraser</li>
+          <li>Daniel Keller</li>
+          <li>Robert Rauschenberg</li>
+        </ul>
       </section>
     </main>
   );
